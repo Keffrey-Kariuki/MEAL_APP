709b341a389bdaf8aba1ad290094366b53ffc1d3
Keffrey <keffkahouse@gmail.com>
2022-10-05 23:02:56 +0300

    Initial commit & fcv

---
+++ app/src/main/java/com/example/mealapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mealapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mealapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvMain) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvMain.setupWithNavController(navController)
+    }
+
+}
